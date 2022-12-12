2e0554c20a2e987e2867dd450efcee3b52d8d722
AbdallahMosa <abdallahmosa95@yahoo.com>
2022-12-12 13:54:36 +0200

    feat : solve the lab25 add new REST api

---
+++ games/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    purchaser= models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description =models.TextField(default="description")
+    img_url=models.TextField(default='please add image url')
+    def __str__(self) :
+        return self.title
---
+++ games/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers  import GameSerializer
+from .models import Game
+# Create your views here.
+class GameListView(ListCreateAPIView):
+    queryset=Game.objects.all()
+    serializer_class= GameSerializer
+class GameDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=Game.objects.all()
+    serializer_class= GameSerializer
